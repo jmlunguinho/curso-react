308e4d2f66a6d5ce436269b71b4f18e80f120bac
Josmi <jmlunguinho@gmail.com>
2020-06-21 19:47:18 -0300

    test: ensure Login present valid if password is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -52,4 +52,14 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe(validationStub.errorMessage)
     expect(passwordStatus.textContent).toBe('*')
   })
+
+  test('Should show password email state if Validation succeeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const passwordStatus = sut.getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Tudo Certo')
+    expect(passwordStatus.textContent).toBe('ok')
+  })
 })
